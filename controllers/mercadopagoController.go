5424e32d541dbc7aa156527a71748adf14e03bef
david83656 <davidechevarrieta2012@gmail.com>
2023-08-02 17:12:17 -0300

    Final

--- controllers/mercadopagoController.go
+++ controllers/mercadopagoController.go
@@ -37,3 +37,21 @@ func PeticiónMp(c *gin.Context) {
 
 	fmt.Println(string(body))
 }
+
+func Preciopostal(codigoPostal int, c *gin.Context) int {
+
+	precios := map[int]int{
+		5000: 1320,
+		5500: 1500,
+		6200: 2000,
+		9000: 1240,
+		9407: 1800,
+		3300: 4000,
+	}
+
+	precio, encontrado := precios[codigoPostal]
+	if encontrado {
+		return precio
+	}
+	return -1
+}
